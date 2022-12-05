2638e34122685d6e85b659a016ae3391f41d408e
andruga225 <ryanworld225@yandex.ru>
2022-12-05 10:57:12 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import './Styles/App.css';
+import Service from "./API/Service";
+import BookItem from "./Components/BookItem";
+import BookList from "./Components/BookList";
+import UnreadBooksPage from "./Pages/UnreadBooksPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+    return (
+        <div>
+            <UnreadBooksPage/>
+        </div>
+    )
 }
 
 export default App;
